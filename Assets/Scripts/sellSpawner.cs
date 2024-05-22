c9563cb3cf8dbb1485b0d6817379522e590fe130
DanielGrimling <b22dangr@student.his.se>
2024-05-22 11:49:09 +0200

    Spawnerrrrrrrrrrrrrrr

--- Assets/Scripts/sellSpawner.cs
+++ Assets/Scripts/sellSpawner.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class sellSpawner : MonoBehaviour
 {
+    [SerializeField] private GameObject sellPointPrefab;
+
+    [SerializeField] private int maxSellPoints = 0;
+
+    [SerializeField] private float spawnerTop;
+
+    [SerializeField] private float spawnerBottom;
+
+    public int currentSellPoints = 0;
+    
+    [SerializeField] float spawnRate;
+    
+    public float elapsedTime = 0.0f;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +28,13 @@ public class sellSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        elapsedTime += Time.deltaTime;
         
+        if (currentSellPoints < maxSellPoints && elapsedTime > spawnRate)
+        {
+            elapsedTime = 0;
+            Instantiate(sellPointPrefab, new Vector3(0, Random.Range(spawnerTop,spawnerBottom), 0), Quaternion.identity);
+            currentSellPoints++;
+        }
     }
 }
