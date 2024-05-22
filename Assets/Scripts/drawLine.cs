6b19c13cab1364ae884d0a241147510a1d1aacc0
Jesper <jesper.seppanen@gmail.com>
2024-05-22 11:04:12 +0200

    LineRenderer gradient
    Added green to red gradient between the first and the last point of the linerenderer

--- Assets/Scripts/drawLine.cs
+++ Assets/Scripts/drawLine.cs
@@ -25,4 +25,16 @@ public class drawLine : MonoBehaviour
             line.SetPosition(i, points[i].position);
         }
     }
+    void Start()
+    {
+        line.material = new Material(Shader.Find("Sprites/Default"));
+        
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(Color.green, 0.0f), new GradientColorKey(Color.red, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+        );
+        line.colorGradient = gradient;
+    }
 }
